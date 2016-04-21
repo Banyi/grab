14977399260d02b71cd506f55a4d9613ffb8420b
BY <BanyiBY@163.com>
2016-04-21 20:00:29 +0800

    grab.py修改为类前提交 “

--- grab.py
+++ grab.py
@@ -1,15 +1,8 @@
 #!/user/bin/env python3
 # _*_ coding: utf-8 _*_
 
-'''
-1.BeautifulSoup4解析html
-2.requests库,抓取页面
-3.class Question,问题
-4.class Answer
-5.加载cookie查看登录状态，判断知乎是否登录，
-  调用islogin(),如果登录成功则继续，若失败则返回登录执行user.py
-'''
 
+import re
 import requests
 import http.cookiejar
 from user import Logging, islogin
@@ -34,9 +27,43 @@ except:
 
 url = "https://www.zhihu.com/question/42180532"
 r = requests.get(url)
-# print('status_code: ', int(r.status_code))
 soup = BeautifulSoup(r.content, 'lxml')
-# print(soup.prettify())
-title = soup.title
+title = soup.title.get_text()
+# print('title: ', title)
+
+# 相关话题
 item_tag = soup.find_all('a', class_="zm-item-tag")
-print('item_tag: ', item_tag)
+item = []
+for i in item_tag:
+    item.append(i.get_text(" ", strip=True))
+# print(item)
+
+# 问题描述
+question_detail = soup.find('div', id='zh-question-detail').get_text()
+
+# 评论人数
+comment = soup.find('a', class_="toggle-comment meta-item").get_text()
+# print('comment:', comment)
+
+# 抓取关注人数
+question_followers = soup.find('div', class_="zg-gray-normal").a
+print('question_followers: ', question_followers)     # None
+
+# 回答人数
+answers_num = soup.find('div', class_="zh-answers-title clearfix").h3.get_text()
+print('answers_num:', answers_num)
+
+# 赞同数
+vote_count = soup.find('button', class_="up").span.get_text()
+print('up_count: ', vote_count)
+
+# 回答者
+author = soup.find('a', class_="author-link").get_text()
+print('author: ', author)
+
+# 答案发布时间
+answer_data = soup.find('a', class_="answer-date-link meta-item").get_text()
+print(answer_data)
+# 答案
+answer = soup.find('div', class_="zm-editable-content clearfix").get_text()
+# print(answer)
